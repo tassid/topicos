97e12863433987bf78b051c13833145240141034
Mac <mac@tassis-MacBook-Air.local>
2025-09-03 14:39:25 -0400

    first commit

---
+++ appdemo/src/test/java/br/edu/utfpr/appdemo/AppdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.utfpr.appdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
